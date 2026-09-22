f1b663662e12b2b271d5cd4b38854aaf3ad8414c
Magali MAI <130312385+galima10@users.noreply.github.com>
2026-09-22 14:57:45 +0200

    feat(resources): :sparkles: Create CRUD basic endpoints for Account, Journey and Reservation resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JourneyModule } from './journey/journey.module';
+import { AccountModule } from './account/account.module';
+import { ReservationModule } from './reservation/reservation.module';
 
 @Module({
-  imports: [],
+  imports: [JourneyModule, AccountModule, ReservationModule],
   controllers: [AppController],
   providers: [AppService],
 })
